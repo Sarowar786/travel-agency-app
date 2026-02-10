acd77373b2372b04b1e80db76b02d0e8607d4426
Sarowar786 <sarowar2287@gmail.com>
2026-02-10 11:04:08 +0600

    middleware loginUrl remove

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,25 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { jwtDecode } from "jwt-decode";
+// import { jwtDecode } from "jwt-decode";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("accessToken")?.value;
-  const loginUrl = new URL("/login", request.url);
+  // const loginUrl = new URL("/login", request.url);
 
   // Redirect to login if token is not present
-  if (!token) {
-    return NextResponse.redirect(loginUrl);
-  }
+  // if (!token) {
+  //   return NextResponse.redirect(loginUrl);
+  // }
 
-  try {
-    const user = jwtDecode(token);
-    console.log(user, "user from middleware");
+  // try {
+  //   const user = jwtDecode(token);
+  //   console.log(user, "user from middleware");
 
-    // check the user role and redirect and restrict here
-  } catch (error) {
-    console.error("Error decoding token:", error);
-    return NextResponse.redirect(loginUrl);
-  }
+  //   // check the user role and redirect and restrict here
+  // } catch (error) {
+  //   console.error("Error decoding token:", error);
+  //   return NextResponse.redirect(loginUrl);
+  // }
 
   // Proceed to the requested route
   return NextResponse.next();
